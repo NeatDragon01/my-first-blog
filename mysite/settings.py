49368fa5b76296abeccf3b60feffd06a2b99c7cd
NeatDragon01 <shogungraue1990@hotmail.com>
2024-09-10 13:00:36 -0500

    adjustments made to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o=1rorkpkerhy4gc5m$ay9cjt@jo64je&-#vygt70k!tc@t13&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
